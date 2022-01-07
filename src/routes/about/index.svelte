42ac3a3d0cf3aa88ef00b1fc0ecacc3ac54657fd
Vincent Hirtz <vincent.hirtz@sapiendo.fr>
2022-01-07 18:46:30 +0100

    problem page about

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -30,6 +30,7 @@
 <div class="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
 	<h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">A propos de moi</h1>
 	<div class="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
+		<p> Vincent Hirtz, Web Developer </p>
 		<p>
 			<iframe
 				src="https://www.linkedin.com/embed/feed/update/urn:li:share:6885190097365454848"
